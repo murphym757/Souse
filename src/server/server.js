ee16a06b4373630146e94d50766ce62466deb878
Maurice Murphy <murphym757@yahoo.com>
2019-12-24 23:56:06 -0500

    Fixed database issue

--- src/server/server.js
+++ src/server/server.js
@@ -19,7 +19,7 @@ const express = require('express'),
 ));
 
 mongoose.Promise = global.Promise;
-mongoose.connect(config.DATABASE_LOCAL, {
+mongoose.connect(config.MONGODB_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: false
